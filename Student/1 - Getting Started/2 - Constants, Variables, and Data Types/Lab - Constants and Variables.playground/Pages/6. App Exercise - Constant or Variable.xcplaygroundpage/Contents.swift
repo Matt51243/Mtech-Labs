cdc63a5b3a3d07d12d95e3e16ced81f0f5d99362
Matthew <matt@Matthews-MacBook-Pro.local>
2021-08-20 10:14:49 -0600

    Moving sSudent Files

---
+++ Student/1 - Getting Started/2 - Constants, Variables, and Data Types/Lab - Constants and Variables.playground/Pages/6. App Exercise - Constant or Variable.xcplaygroundpage/Contents.swift
@@ -0,0 +1,10 @@
+let name = "Matthew"
+var age = 17
+var numberOfSteps = 2000
+let stepsGoal = 10_000
+var avgHeartRate = 107
+/*:
+ Now go back and add a line after each constant or variable declaration. On those lines, print a statement explaining why you chose to declare the piece of information as a constant or variable.
+ 
+[Previous](@previous)  |  page 6 of 10  |  [Next: Exercise - Types and Type Safety](@next)
+ */
---
+++ Student/1 - Getting Started/4 - Control Flow/Lab - Control Flow.playground/Pages/8. Exercise - Ternary Operator.xcplaygroundpage/Contents.swift
@@ -0,0 +1,22 @@
+/*:
+## Exercise - Ternary Operator
+ 
+ Refactor the code below so that `largest` is declared and assigned to in one line using the ternary operator.
+ */
+
+
+
+var largest: Int
+let number1 = 14
+let number2 = 25
+largest = number1 < number2 ? number2 : number1
+print(largest)
+
+/* if number1 < number2 {
+largest = number2
+} else {
+ largest = number1
+ }*/
+/*:
+[Previous](@previous)  |  page 8 of 9  |  [Next: App Exercise - Ternary Messages](@next)
+ */
